5439c14cef3260e2f0b5d30b7c92aea9741dd293
Fanjun Yu <fyu5017@conestogac.on.ca>
2025-02-21 18:10:10 -0500

    add Healthcare_And_Wellness2

---
+++ Healthcare_And_Wellness2/Healthcare_And_Wellness/Healthcare_And_Wellness/Controllers/AdministratorController.cs
@@ -0,0 +1,112 @@
+﻿using Healthcare_And_Wellness.Data;
+using Healthcare_And_Wellness.Models;
+using Healthcare_And_Wellness.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.EntityFrameworkCore;
+
+namespace Healthcare_And_Wellness.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        private ManagementContext _managementContext;
+
+        public AdministratorController(ManagementContext managementContext)
+        {
+            _managementContext = managementContext;
+        }
+
+        private void PopulateViewBag()
+        {
+            ViewBag.Username = HttpContext.Session.GetString("Username");
+            ViewBag.Role = HttpContext.Session.GetString("Role");
+        }
+
+        public IActionResult ListJobs()
+        {
+            PopulateViewBag();
+            List<Job> jobs = _managementContext.jobs.ToList();
+            return View(jobs);
+        }
+
+        [HttpGet]
+        public IActionResult AddJobs(int id)
+        {
+            PopulateViewBag();
+            return View(new Job() { jobID = id });
+        }
+
+        [HttpPost]
+        public IActionResult AddJobs(Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                _managementContext.jobs.Add(job);
+                _managementContext.SaveChanges();
+                return RedirectToAction("ListJobs", "Administrator");
+            }
+            return View(job);
+        }
+
+        public IActionResult JobPage(int id)
+        {
+            PopulateViewBag();
+            List<Applicant> users = _managementContext.applicants.Include(m => m.Job).Where(m => m.jobID == id).ToList();
+            List<Job> jobs = _managementContext.jobs.Where(m => m.jobID == id).ToList();
+            var jobViewModel = new JobViewModel() { Job = jobs.FirstOrDefault(), applicants = users };
+
+            return View(jobViewModel);
+        }
+
+        public IActionResult SendGmailAccept(int id)
+        {
+            List<Applicant> users = _managementContext.applicants.Include(m => m.Job).Where(m => m.userID == id).ToList();
+            if (!users.Any()) return NotFound();
+
+            var user = users.First();
+            SendEmail(user.emailUser, "Congratulations!", "You have been accepted.");
+            user.statusUser = "Accepted";
+            _managementContext.applicants.Update(user);
+            _managementContext.SaveChanges();
+
+            return RedirectToAction("JobPage", "Administrator", new { id = user.jobID });
+        }
+
+        public IActionResult SendGmailDeny(int id)
+        {
+            List<Applicant> users = _managementContext.applicants.Include(m => m.Job).Where(m => m.userID == id).ToList();
+            if (!users.Any()) return NotFound();
+
+            var user = users.First();
+            SendEmail(user.emailUser, "Job Application Update", "Unfortunately, you have been denied.");
+            user.statusUser = "Denied";
+            _managementContext.applicants.Update(user);
+            _managementContext.SaveChanges();
+
+            return RedirectToAction("JobPage", "Administrator", new { id = user.jobID });
+        }
+
+        private void SendEmail(string toAddress, string subject, string body)
+        {
+            string fromAddress = "your-email@gmail.com";
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromAddress, "your-app-password"),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage()
+            {
+                From = new MailAddress(fromAddress),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+            mailMessage.To.Add(toAddress);
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
---
+++ Healthcare_And_Wellness2/Healthcare_And_Wellness/Healthcare_And_Wellness/Controllers/ApplicantController.cs
@@ -0,0 +1,86 @@
+﻿using Healthcare_And_Wellness.Data;
+using Healthcare_And_Wellness.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Healthcare_And_Wellness.Controllers
+{
+    public class ApplicantController : Controller
+    {
+        private ManagementContext _managementContext;
+
+        public ApplicantController(ManagementContext managementContext)
+        {
+            _managementContext = managementContext;
+        }
+
+        private void PopulateViewBag()
+        {
+            ViewBag.Username = HttpContext.Session.GetString("Username");
+            ViewBag.Role = HttpContext.Session.GetString("Role");
+        }
+
+        public IActionResult ListJobs()
+        {
+            PopulateViewBag();
+            List<Job> jobs = _managementContext.jobs.ToList();
+            return View(jobs);
+        }
+
+        [HttpGet]
+        public IActionResult AddUser(int id)
+        {
+            PopulateViewBag();
+            return View(new Applicant() { jobID = id });
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(Applicant user)
+        {
+            //PopulateViewBag();
+            //if (ModelState.IsValid)
+            //{
+            //    _managementContext.applicants.Add(user);
+            //    _managementContext.SaveChanges();
+
+            //    var job = _managementContext.jobs.Find(user.jobID);
+            //    if (job != null)
+            //    {
+            //        job.statusJob = "Applied";
+            //        _managementContext.jobs.Update(job);
+            //        _managementContext.SaveChanges();
+            //    }
+
+            //    return RedirectToAction("ListJobs", "Applicant");
+            //}
+            //return View(user);
+
+            PopulateViewBag();
+            if (ModelState.IsValid)
+            {
+                // Simply add the new applicant without checking if they already applied
+                _managementContext.applicants.Add(user);
+                _managementContext.SaveChanges();
+
+                // Optionally, if you want to track if a job has any applicants or keep it as "Apply"
+                // you could leave the status of the job unchanged or set it back to "Apply" each time a new applicant applies.
+                var job = _managementContext.jobs.Find(user.jobID);
+                if (job != null)
+                {
+                    // If you want to allow applications continuously, we don’t change the job status to "Applied"
+                    // Just ensure the job is still available for more applicants
+                    if (job.statusJob != "Apply")
+                    {
+                        job.statusJob = "Apply";  // Keeps it open for more applications
+                        _managementContext.jobs.Update(job);
+                        _managementContext.SaveChanges();
+                    }
+                }
+
+                // Redirect back to the job list to continue applying
+                return RedirectToAction("ListJobs", "Applicant");
+            }
+            return View(user);
+        }
+    }
+}
